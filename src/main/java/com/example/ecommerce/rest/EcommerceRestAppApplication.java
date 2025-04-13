ad128a8a0ea4d2b91ed7eec8f6ff376969e8a78e
rohit sachan <rohitsachan333@gmail.com>
2025-04-13 14:15:55 +0530

    Initial commit

---
+++ src/main/java/com/example/ecommerce/rest/EcommerceRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce/rest/EcommerceRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
